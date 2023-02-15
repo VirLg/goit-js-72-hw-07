7616d8a4513c2c66b3cb0fbed049db051079aa07
VirLg <108211942+VirLg@users.noreply.github.com>
2023-02-15 21:47:25 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refUl = document.querySelector('.gallery')
+// refUl.addEventListener('click', onCloseModalWindow)
+
+function createItem({original, preview, description}) { 
+        return `
+    <a class="gallery__item"
+        href="${original}" onclick="return false">
+        <img class="gallery__image"
+        src="${preview}" 
+        alt="${description}" />
+    </a>         `      
+};
+
+
+const markupGallary= galleryItems.map(createItem).join('')
+
+refUl.insertAdjacentHTML("afterbegin", markupGallary)
+
+
+
+
+let gallery = new SimpleLightbox('.gallery a',
+    {
+        navText: ['←', '→'],
+        captionsData: 'alt',
+        animationSpeed: 1800,
+        animationSlide: true,
+        // swipeTolerance:50,
+        // fadeSpeed:1200,
+        captionDelay:250,
+    });
+
+
+// gallery.on('show.simplelightbox', function () {
+
+// 	// Do something…
+// });
+
+// gallery.on('error.simplelightbox', function (e) {
+// 	console.log(e); // Some usefull information
+// });
+
